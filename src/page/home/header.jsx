956c87287a67386c1580a86b1876ef0801c194d5
ashrafwbst <er.ashrafkamal@gmail.com>
2023-04-11 14:04:07 +0530

    updated poker ui of wpt

--- src/page/home/header.jsx
+++ src/page/home/header.jsx
@@ -1,9 +1,9 @@
-import { useState }  from 'react'
+import { useState } from 'react'
 // import { useHistory } from 'react-router-dom';
 import numFormatter from "../../utils/utils";
 import token from "../../assets/coin.png";
 import tickets from "../../assets/tickets.png";
-import { Button,OverlayTrigger } from "react-bootstrap";
+import { Button, OverlayTrigger } from "react-bootstrap";
 import { Tooltip } from "react-bootstrap";
 import logo from "../../assets/game/logo.png";
 import { FaQuestionCircle } from "react-icons/fa";
@@ -14,8 +14,8 @@ import { authInstance } from '../../utils/axios.config';
 import Register from './registerPage';
 import { useContext } from 'react';
 import UserContext from '../../context/UserContext';
-const Header = ({userData, handleShow}) => {
-   const {user,setUser}=useContext(UserContext)
+const Header = ({ userData, handleShow }) => {
+    const { user, setUser } = useContext(UserContext)
     const renderWallet = (props) => (
         <Tooltip id="button-tooltip" {...props}>
             This is your token balance, and can be used for betting.
@@ -26,65 +26,65 @@ const Header = ({userData, handleShow}) => {
             This is your ticket balance and can be redeemed for prizes.
         </Tooltip>
     );
-const [account, setAccount] = useState(null);
-//const [balance, setBalance] = useState(null);
-const [openRegister,setOpenRegister]=useState(false)
-// const accountsChanged = async (newAccount) => {
-//     setAccount(newAccount);
-//     try {
-//       const balance = await window.ethereum.request({
-//         method: "eth_getBalance",
-//         params: [newAccount.toString(), "latest"],
-//       });
-//       console.log("balance-->",balance)
-//     //   setBalance(ethers.utils.formatEther(balance));
-//     } catch (err) {
-//       console.error(err);
-//       toast.error("There was a problem connecting to MetaMask",{toastId:"connect"})
-//     }
-//   };
-// useEffect(() => {
-//     if (window.ethereum) {
-//       window.ethereum.on("accountsChanged", accountsChanged);
-//       window.ethereum.on("chainChanged", chainChanged);
-//     }
-//   }, []);
-  const connectHandler = async () => {
-    try { 
-        if (window.ethereum) {
-        const res = await window.ethereum.request({
-          method: "eth_requestAccounts",
-        });
-        if(res[0]){
-            setAccount(res[0])
-         const resp=await authInstance().post('/loginWithMetamask',{
-            metaMaskAddress:res[0]
-         })
-         console.log("Response--->",resp)
-         const {status,message,token,user}=resp?.data
-         if(status===200){
-            setUser(user)
-            localStorage.setItem('token',token)
-         }
-         if(status===401){
-            toast.error(message,{toastId:"toastId"})
-            setOpenRegister(true)
-         }
+    const [account, setAccount] = useState(null);
+    //const [balance, setBalance] = useState(null);
+    const [openRegister, setOpenRegister] = useState(false)
+    // const accountsChanged = async (newAccount) => {
+    //     setAccount(newAccount);
+    //     try {
+    //       const balance = await window.ethereum.request({
+    //         method: "eth_getBalance",
+    //         params: [newAccount.toString(), "latest"],
+    //       });
+    //       console.log("balance-->",balance)
+    //     //   setBalance(ethers.utils.formatEther(balance));
+    //     } catch (err) {
+    //       console.error(err);
+    //       toast.error("There was a problem connecting to MetaMask",{toastId:"connect"})
+    //     }
+    //   };
+    // useEffect(() => {
+    //     if (window.ethereum) {
+    //       window.ethereum.on("accountsChanged", accountsChanged);
+    //       window.ethereum.on("chainChanged", chainChanged);
+    //     }
+    //   }, []);
+    const connectHandler = async () => {
+        try {
+            if (window.ethereum) {
+                const res = await window.ethereum.request({
+                    method: "eth_requestAccounts",
+                });
+                if (res[0]) {
+                    setAccount(res[0])
+                    const resp = await authInstance().post('/loginWithMetamask', {
+                        metaMaskAddress: res[0]
+                    })
+                    console.log("Response--->", resp)
+                    const { status, message, token, user } = resp?.data
+                    if (status === 200) {
+                        setUser(user)
+                        localStorage.setItem('token', token)
+                    }
+                    if (status === 401) {
+                        toast.error(message, { toastId: "toastId" })
+                        setOpenRegister(true)
+                    }
+                }
+
+            } else {
+                toast.error("Install MetaMask", { toastId: "Install meta" })
+            }
+        } catch (err) {
+            console.error(err);
+            toast.error("There was a problem connecting to MetaMask")
         }
-      
-    } else {
-        toast.error("Install MetaMask",{toastId:"Install meta"})  
-    }
-} catch (err) {
-    console.error(err);
-    toast.error("There was a problem connecting to MetaMask")
-  }
-  };
-  
-//   const chainChanged = () => {
-//     setAccount(null);
-//     setBalance(null);
-//   };
+    };
+
+    //   const chainChanged = () => {
+    //     setAccount(null);
+    //     setBalance(null);
+    //   };
 
     return (
         <div className="user-header">
@@ -96,7 +96,7 @@ const [openRegister,setOpenRegister]=useState(false)
                         </a>
                     </div>
                     <div className="create-game-box">
-                        {user?<><a href={`${landingClient}profile`}>
+                        {user ? <><a href={`${landingClient}profile`}>
                             <div className="create-game-box-avtar">
                                 <img
                                     src={
@@ -108,52 +108,52 @@ const [openRegister,setOpenRegister]=useState(false)
                                 <h5>{user?.username}</h5>
                             </div>
                         </a>
-                        <div className="walletTicket-box">
-                            <div className="pokerWallet-box">
-                                <img src={token} alt="" className="pokerWallet" />
-                                <span>{numFormatter(user?.wallet || 0)}</span>
-                                <OverlayTrigger
-                                   placement={window.innerWidth < 767 ? "right":"left"}
-                                    delay={{ show: 250, hide: 400 }}
-                                    overlay={renderWallet}
-                                >
-                                    <Button variant="success">
-                                        <FaQuestionCircle />
-                                    </Button>
-                                </OverlayTrigger>
-                            </div>
-                            <div className="pokerWallet-box">
-                                <img src={tickets} alt="" className="pokerWallet" />
-                                <span>{numFormatter(user?.ticket || 0)}</span>
-                                <OverlayTrigger
-                                    placement={window.innerWidth < 767 ? "right":"left"}
-                                    delay={{ show: 250, hide: 400 }}
-                                    overlay={renderTicket}
-                                >
-                                    <Button variant="success">
-                                        <FaQuestionCircle />
-                                    </Button>
-                                </OverlayTrigger>
-                            </div>
-                        </div><button
-                            type="button"
-                            className="create-game-boxBtn"
-                            onClick={handleShow}
-                        >
-                            Create Game
-                        </button></>:
-                        <>
-                        <button type="button"
-                                className="create-game-boxBtn" onClick={() => connectHandler()}>Connect Metamask</button>
-                        
-                           
-                        </>
-                        }   
+                            <div className="walletTicket-box">
+                                <div className="pokerWallet-box">
+                                    <img src={token} alt="" className="pokerWallet" />
+                                    <span>{numFormatter(user?.wallet || 0)}</span>
+                                    <OverlayTrigger
+                                        placement={window.innerWidth < 767 ? "right" : "left"}
+                                        delay={{ show: 250, hide: 400 }}
+                                        overlay={renderWallet}
+                                    >
+                                        <Button variant="success">
+                                            <FaQuestionCircle />
+                                        </Button>
+                                    </OverlayTrigger>
+                                </div>
+                                <div className="pokerWallet-box">
+                                    <img src={tickets} alt="" className="pokerWallet" />
+                                    <span>{numFormatter(user?.ticket || 0)}</span>
+                                    <OverlayTrigger
+                                        placement={window.innerWidth < 767 ? "right" : "left"}
+                                        delay={{ show: 250, hide: 400 }}
+                                        overlay={renderTicket}
+                                    >
+                                        <Button variant="success">
+                                            <FaQuestionCircle />
+                                        </Button>
+                                    </OverlayTrigger>
+                                </div>
+                            </div><button
+                                type="button"
+                                className="create-game-boxBtn"
+                                onClick={handleShow}
+                            >
+                                Create Game
+                            </button></> :
+                            <>
+                                <button type="button"
+                                    className="create-game-boxBtn" onClick={() => connectHandler()}>Connect Metamask</button>
+
+
+                            </>
+                        }
                     </div>
-                    
+
                 </div>
             </div>
-            <Register openRegister={openRegister} setOpenRegister={setOpenRegister} account={account}/>
+            <Register openRegister={openRegister} setOpenRegister={setOpenRegister} account={account} />
         </div>
 
     )
--- src/page/home/leaderBoard.jsx
+++ src/page/home/leaderBoard.jsx
@@ -21,7 +21,7 @@ let userId;
 const customStyles = {
   option: (provided) => ({
     ...provided,
-    background: "#1b1b1b",
+    background: "#ebe8e8",
     color: "#ddd",
     fontWeight: "400",
     fontSize: "16px",
@@ -30,37 +30,38 @@ const customStyles = {
     cursor: "pointer",
     borderRadius: "4px",
     borderBottom: "1px solid #141414",
+    textAlign: "left",
     ":hover": {
-      background: "#141414",
+      background: "#ebe8e8",
       borderRadius: "4px",
     },
   }),
   menu: (provided) => ({
     ...provided,
-    background: "#1b1b1b",
+    background: "#ebe8e8",
     borderRadius: "30px",
     padding: "10px 20px",
     border: "2px solid transparent",
   }),
   control: () => ({
-    background: "#1b1b1b",
-    border: "2px solid #1b1b1b",
+    background: "#ebe8e8",
+    border: " 1px solid #ebe8e8",
     borderRadius: "30px",
-    color: "#fff",
+    color: "#000",
     display: "flex",
     alignItem: "center",
     height: "41",
     margin: "2px 0",
-    boxShadow: " 0 2px 10px #000000a5",
+    boxShadow: "  0 0px 10px #c0b0b0a1",
     cursor: "pointer",
     ":hover": {
-      background: "#1b1b1b",
+      background: "#ebe8e8",
       // border: "2px solid #306CFE",
     },
   }),
   singleValue: (provided) => ({
     ...provided,
-    color: "#fff",
+    color: "#000",
     fontWeight: "400",
     fontSize: "14px",
     lineHeight: "16px",
@@ -75,11 +76,13 @@ const customStyles = {
     fontSize: "14px",
     lineHeight: "19px",
     color: "#858585c7",
+
   }),
   input: (provided) => ({
     ...provided,
     // height: "38px",
-    color: "fff",
+    color: "#000",
+
   }),
   valueContainer: (provided) => ({
     ...provided,
@@ -88,13 +91,13 @@ const customStyles = {
   indicatorsContainer: (provided) => ({
     ...provided,
     paddingRight: "20px",
-    color: "#858585c7",
+    color: '#858585c7',
   }),
   svg: (provided) => ({
     ...provided,
-    fill: "#858585c7 !important",
+    fill: '#858585c7 !important',
     ":hover": {
-      fill: "#858585c7 !important",
+      fill: '#858585c7 !important',
     },
   }),
 };
