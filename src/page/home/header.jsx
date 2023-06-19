18c5473b82efb12f6833fd8b93a52a87e4af3d26
farhan-dev <syedwebsul123@gmmail.com>
2023-06-19 17:57:17 +0530

    header nav bar

--- src/page/home/header.jsx
+++ src/page/home/header.jsx
@@ -4,6 +4,9 @@ import numFormatter from "../../utils/utils";
 import token from "../../assets/coin.png";
 import deposit from "../../assets/deposit.png";
 import withdraw from "../../assets/withdraw.png";
+import menu from "../../assets/menu.svg";
+import close from "../../assets/closecross.svg";
+
 import {
   Button,
   ButtonGroup,
@@ -60,6 +63,9 @@ const Header = ({ userData, handleShow, handleDeposit, handleWithdraw }) => {
     label: "USDT",
   });
   const [notificationCount, setNotificationCount] = useState(0);
+
+  const [show, setShow] = useState(false);
+  const handleclickShow = () => setShow(!show);
 
   // console.log("currencyType ==>", currencyType);
 
@@ -234,19 +240,76 @@ const Header = ({ userData, handleShow, handleDeposit, handleWithdraw }) => {
 
   return (
     <>
-      <div className="user-header">
-        <div className="user-header-grid">
-          <div className="casino-logo">
+      <div className='user-header'>
+        <div className='user-header-grid'>
+          <div className='casino-logo'>
             <a href={clientUrl}>
-              <img src={logo} alt="" />
+              <img src={logo} alt='' />
             </a>
           </div>
-          <div className="create-game-box">
+
+          {!user && (
+            <div className='header-navlink-desktop-content'>
+              <div class='navLink'>
+                <ul>
+                  <li>
+                    <a href='https://acepoker.io/'>Homepage</a>
+                  </li>
+                  <li>
+                    <a href='https://acepoker.io/#about-us'>About us</a>
+                  </li>
+                  <li>
+                    <a href='https://acepoker.io/#playPoker'>Play Poker</a>
+                  </li>
+                  <li>
+                    <a href='https://acepoker.io/#WPT_Token'>ACE Token</a>
+                  </li>
+                  <li>
+                    <a href='https://docs.acepoker.io'>Docs</a>
+                  </li>
+                  <li class='ddparent'>
+                    <a href='#'>Security &amp; Privacy</a>
+                    <span class='menuDdIcon'>
+                      <img
+                        src='https://acepoker.io/wp-content/themes/harsol/assets/images/PrivacyIcon.svg'
+                        alt='...'
+                      />
+                    </span>
+                    <ul class='ddstyle'>
+                      <li>
+                        <a href='https://acepoker.io/privacy/'>
+                          Privacy Policy
+                        </a>
+                      </li>
+                      <li>
+                        <a href='https://acepoker.io/cookie-policy-2/'>
+                          Cookie Policy
+                        </a>
+                      </li>
+                      <li>
+                        <a href='https://acepoker.io/tearm-of-use/'>
+                          Tearm of use
+                        </a>
+                      </li>
+                    </ul>
+                  </li>
+                  <li>
+                    <a href='https://acepoker.io/service/'>Services</a>
+                  </li>
+                  <li>
+                    <a href='https://acepoker.io/#footer'>Contact</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          )}
+
+          <div className='create-game-box'>
             {user ? (
               <>
                 {/* <a href={`${landingClient}profile`}> */}
 
-                <div className="create-game-box-avtar">
+                <div className='create-game-box-avtar'>
                   {/* <img
                                     src={
                                         user?.profile ||
@@ -258,7 +321,7 @@ const Header = ({ userData, handleShow, handleDeposit, handleWithdraw }) => {
                   <h5>{user?.username}</h5>
                 </div>
                 <div
-                  className="create-game-box-avtar"
+                  className='create-game-box-avtar'
                   onClick={handleNotifiationShow}
                 >
                   <FaBell />
@@ -266,11 +329,11 @@ const Header = ({ userData, handleShow, handleDeposit, handleWithdraw }) => {
                   {showNotifications ? <Notifications /> : null}
                 </div>
                 {/* </a> */}
-                <div className="walletTicket-box">
+                <div className='walletTicket-box'>
                   <Dropdown as={ButtonGroup}>
                     <Button>
-                      <div className="pokerWallet-box">
-                        <img src={token} alt="" className="pokerWallet" />
+                      <div className='pokerWallet-box'>
+                        <img src={token} alt='' className='pokerWallet' />
                         <span>
                           {numFormatter(user?.wallet.toFixed(2) || 0)}
                         </span>
@@ -285,8 +348,8 @@ const Header = ({ userData, handleShow, handleDeposit, handleWithdraw }) => {
                                         </OverlayTrigger> */}
                       </div>
                     </Button>
-                    <Dropdown.Toggle split id="dropdown-split-basic" />
-                    <Dropdown.Menu flip="True" align="start">
+                    <Dropdown.Toggle split id='dropdown-split-basic' />
+                    <Dropdown.Menu flip='True' align='start'>
                       <Dropdown.Item onClick={handleShowDepositPopUp}>
                         Deposit
                       </Dropdown.Item>
@@ -311,15 +374,15 @@ const Header = ({ userData, handleShow, handleDeposit, handleWithdraw }) => {
                                     </div> */}
                 </div>
                 <button
-                  type="button"
-                  className="create-game-boxBtn"
+                  type='button'
+                  className='create-game-boxBtn'
                   onClick={handleShow}
                 >
                   Create Game
                 </button>
                 <button
-                  type="button"
-                  className="create-game-boxBtn"
+                  type='button'
+                  className='create-game-boxBtn'
                   onClick={handleLogOut}
                 >
                   Logout
@@ -328,17 +391,93 @@ const Header = ({ userData, handleShow, handleDeposit, handleWithdraw }) => {
             ) : (
               <>
                 <button
-                  type="button"
-                  className="connect-meta red-btnPrimary"
+                  type='button'
+                  className='connect-meta red-btnPrimary'
                   onClick={() =>
                     connectWithMetamask({ chainId: ChainId.Arbitrum })
                   }
                 >
                   Connect
                 </button>
+                <div className='menubtn' onClick={handleclickShow}>
+                  {show ? (
+                    <img src={close} alt='close' />
+                  ) : (
+                    <img src={menu} alt='menu' />
+                  )}
+                </div>
               </>
             )}
             {/* {ChainId.ArbitrumGoerli} */}
+          </div>
+
+          <div className={`navCol-responsive ${show ? `active` : ``}`}>
+            {/* <div class='navCol-responsive'> */}
+            <div class='row align-items-center'>
+              <div class='col-lg'>
+                <div class='navLink-content'>
+                  <ul>
+                    <li>
+                      <a href='https://acepoker.io/'>Homepage</a>
+                    </li>
+                    <li>
+                      <a href='https://acepoker.io/#about-us'>About us</a>
+                    </li>
+                    <li>
+                      <a href='https://acepoker.io/#playPoker'>Play Poker</a>
+                    </li>
+                    <li>
+                      <a href='https://acepoker.io/#WPT_Token'>ACE Token</a>
+                    </li>
+                    <li>
+                      <a href='https://docs.acepoker.io'>Docs</a>
+                    </li>
+                    <li class='ddparent'>
+                      <a href='#'>Security &amp; Privacy</a>
+                      <span class='menuDdIcon'>
+                        <img
+                          src='https://acepoker.io/wp-content/themes/harsol/assets/images/PrivacyIcon.svg'
+                          alt='...'
+                        />
+                      </span>
+                      <ul class='ddstyle'>
+                        <li>
+                          <a href='https://acepoker.io/privacy/'>
+                            Privacy Policy
+                          </a>
+                        </li>
+                        <li>
+                          <a href='https://acepoker.io/cookie-policy-2/'>
+                            Cookie Policy
+                          </a>
+                        </li>
+                        <li>
+                          <a href='https://acepoker.io/tearm-of-use/'>
+                            Tearm of use
+                          </a>
+                        </li>
+                      </ul>
+                    </li>
+                    <li>
+                      <a href='https://acepoker.io/service/'>Services</a>
+                    </li>
+                    <li>
+                      <a href='https://acepoker.io/#footer'>Contact</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div class='col-lg-auto d-sm-none  d-xl-block'>
+                <div class='headerRightCol'>
+                  <a
+                    href='https://app.acepoker.io/'
+                    class='btn btnPrimary minWdBtn'
+                  >
+                    Connect
+                  </a>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
         <Register
@@ -395,11 +534,11 @@ const DepositModal = ({
     <Modal
       show={showTransactionModal}
       centered
-      className="transaction-modalPopup fade casino-popup"
+      className='transaction-modalPopup fade casino-popup'
     >
       <Form.Label>Currency Type</Form.Label>
-      <Modal.Body className="transaction-validatingTranction">
-        <img src={deposit} alt="deposit" />
+      <Modal.Body className='transaction-validatingTranction'>
+        <img src={deposit} alt='deposit' />
         {/* <Form.Label>Currency type</Form.Label>
                 <Form.Select size="sm">
                     <option>USDT</option>
@@ -413,16 +552,16 @@ const DepositModal = ({
         />
         <Form.Label>Deposit USDT</Form.Label>
         <Form.Control
-          name="Deposit"
+          name='Deposit'
           onInput={handleDepositAmt}
           value={depositAmt}
-          type="number"
-          placeholder="Ex : 50"
+          type='number'
+          placeholder='Ex : 50'
         />
       </Modal.Body>
       <Modal.Footer>
         <Button
-          variant="secondary"
+          variant='secondary'
           onClick={() => {
             setShowTransactionModal(false);
           }}
@@ -430,12 +569,12 @@ const DepositModal = ({
           Close
         </Button>
         <Button
-          variant="primary"
-          type="submit"
+          variant='primary'
+          type='submit'
           onClick={handleDepositTransaction}
           disabled={showSpinner}
         >
-          {showSpinner ? <Spinner animation="border" /> : "Deposit"}
+          {showSpinner ? <Spinner animation='border' /> : "Deposit"}
         </Button>
       </Modal.Footer>
     </Modal>
@@ -456,23 +595,23 @@ const WithdrawlModal = ({
     <Modal
       show={showWithdrawTransaction}
       centered
-      className="transaction-modalPopup fade casino-popup"
+      className='transaction-modalPopup fade casino-popup'
     >
-      <Modal.Body className="transaction-validatingTranction">
-        <img src={withdraw} alt="" />
+      <Modal.Body className='transaction-validatingTranction'>
+        <img src={withdraw} alt='' />
         <Form.Label>Withdraw USDT</Form.Label>
         <Form.Control
-          name="Deposit"
+          name='Deposit'
           onInput={handleWithdrawAmt}
           // onKeyDown={handleWithdrawAmt}
           value={withdrawAmt}
-          type="text"
-          placeholder="Ex : 50"
+          type='text'
+          placeholder='Ex : 50'
         />
       </Modal.Body>
       <Modal.Footer>
         <Button
-          variant="secondary"
+          variant='secondary'
           onClick={() => {
             setShowWithdrawTransaction(false);
           }}
@@ -480,12 +619,12 @@ const WithdrawlModal = ({
           Close
         </Button>
         <Button
-          variant="primary"
-          type="submit"
+          variant='primary'
+          type='submit'
           onClick={handleWithdrawTransaction}
           disabled={showSpinner}
         >
-          {showSpinner ? <Spinner animation="border" /> : "Withdraw"}
+          {showSpinner ? <Spinner animation='border' /> : "Withdraw"}
         </Button>
       </Modal.Footer>
     </Modal>
